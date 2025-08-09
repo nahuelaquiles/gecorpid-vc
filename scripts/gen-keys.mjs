28302e88bf56f8dc77cd8d7c5a46b698f68edde0
Nahuel Aquiles Garcia <gecorpinfo@gmail.com>
2025-08-09 16:53:35 -0300

    initial push

---
+++ scripts/gen-keys.mjs
@@ -0,0 +1,16 @@
+﻿import { generateKeyPair, exportJWK } from "jose";
+
+// Genera Ed25519 (EdDSA) y marca las claves como "extractable"
+const { publicKey, privateKey } = await generateKeyPair("EdDSA", {
+  crv: "Ed25519",
+  extractable: true
+});
+
+const pub  = await exportJWK(publicKey);
+const priv = await exportJWK(privateKey);
+
+pub.alg  = "EdDSA"; pub.use  = "sig";  // metadatos útiles
+priv.alg = "EdDSA"; priv.use = "sig";
+
+console.log("PUBLIC_JWK:\\n",  JSON.stringify(pub,  null, 2));
+console.log("PRIVATE_JWK:\\n", JSON.stringify(priv, null, 2));
